4cbaad3c35d6c8ba2cc2fdb2408a33c8fa0c952f
gibbonsal <gibbonsal@gmail.com>
2022-01-05 00:39:27 -0800

    Update app.js
    Edited typo.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,6 @@
 //UFO-level-1
 
-//1. * Using the UFO dataset provided in the form of an array of JavaScript objects, 
+//1. Using the UFO dataset provided in the form of an array of JavaScript objects, 
 //write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
 
